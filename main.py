f8267b7ca8c9ae2d2edee7bc8a01cb530a13a839
Anna Khoriakova <khoriakova.anna@gmail.com>
2026-08-21 10:38:23 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Name: Anna Khoriakova
+Date: 2026-08-21
+Description: This program demonstrates neural network models for property value prediction 
+             using the City of Calgary property assessment dataset.
+             Includes examples of underfitting, overfitting, and an optimized model.
+'''
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
